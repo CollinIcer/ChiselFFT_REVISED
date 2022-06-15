f213ecdd8da34f2a0db2b26f7eab83fe6d8a22d0
CollinIcer <huangxke@yahoo.com>
2022-06-15 18:08:41 +0800

    Update FFT 128_orderin.py

--- FFT 128_orderin.py
+++ FFT 128_orderin.py
@@ -65,8 +65,10 @@ def Butterfly4(in1,in2,in3,in4):
     return(c0,c1,c2,c3)
 
 fft_in = []
-#for i in range (0,128):
-#    fft_in.append(complex(i,i)) #DW IN = 12
+fft_orderdata = []
+for i in range (0,128):
+    fft_orderdata.append(complex(i,i)) #DW IN = 12
+    #fft_in.append(complex(i,i)) #DW IN = 12
 
 fft_in.append(complex(52,54       ) )
 fft_in.append(complex(42,-53      ) )
@@ -222,19 +224,58 @@ def fft128(fft_in):
             res = Butterfly2(fft_in[i*128+j],fft_in[i*128+j+64])
             stg0_bf[i*128+j]     = res[0]
             stg0_bf[i*128+j+64]  = res[1]
+    print("stg0 bf res0-64",stg0_bf[0:64])
+    print("stg0 bf res64-128",stg0_bf[64:128])
+
 
     #print(stg0_bf)
     stg0_tw =[]
+    stg0_wnid =[]
+    stg0_wn =[]
     for i in range(0,FFTLength):
         lut_adr_l = i & (int)(2**(math.log(128,2)-1)-1) 
         lut_adr_h = (i >> (int)(math.log(128,2)-1)) & 0x1 #1bit
         lut_adr = lut_adr_h * lut_adr_l 
         #print("stg2tw",lut_adr)
         wn = wn_lut((int)(math.log(FFTLength/128,2)),lut_adr) #64 depth
-        #print(wn)
-        stg0_tw.append(ComplexMul(stg0_bf[i],wn,DW_IN+2))
+        #if(i>=64):
+        #    print("stg0_wn ",i,"is" ,wn)
+        #    print("stg0 bf",stg0_bf[i])
+        stg0_tw.append(ComplexMul(stg0_bf[i],wn,DW_IN+1))
+        stg0_wnid.append(lut_adr)
+        stg0_wn.append(wn)
 
-    #print(stg0_tw)
+    #print("stg0 wn 0",stg0_wn[64:128:4])
+    #print("stg0 tw 0",stg0_tw[64:128:4])
+    #print("stg0 tw 1",stg0_tw[65:128:4])
+    #print("stg0 tw 2",stg0_tw[66:128:4])
+    #print("stg0 tw 3",stg0_tw[67:128:4])
+
+    #print("stg0 wnid",stg0_wnid)
+    #print("stg0 wn 0 ",stg0_wn[64:128:4])
+    #print("stg0 wn 1 ",stg0_wn[65:128:4])
+    #print("stg0 wn 2 ",stg0_wn[66:128:4])
+    #print("stg0 wn 3 ",stg0_wn[67:128:4])
+    
+    idx=0;
+    for i in stg0_wn[64:128:4]:
+        #print(idx, " : stg1_twi_lut0 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg0_wn[65:128:4]:
+        #print(idx, " : stg1_twi_lut1 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg0_wn[66:128:4]:
+        #print(idx, " : stg1_twi_lut2 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg0_wn[67:128:4]:
+        #print(idx, " : stg1_twi_lut3 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
 
      #stg2 bf4
     stg1_bf = [0]*128
@@ -245,15 +286,50 @@ def fft128(fft_in):
             stg1_bf[i*64+j+16]  = res[1]
             stg1_bf[i*64+j+32]  = res[2]
             stg1_bf[i*64+j+48]  = res[3]
+            #if(i==1):
+                #print("stg2 bf res in ","idx is",j, stg0_tw[i*64+j],stg0_tw[i*64+j+16],stg0_tw[i*64+j+32],stg0_tw[i*64+j+48])
+                #print("stg2 bf res    ","idx is",j, res[0], res[1], res[2], res[3])
+    #print("stg2 bf:",stg1_bf)
+
 
     stg1_tw =[]
+    stg1_wnid =[]
+    stg1_wn =[]
     for i in range(0,FFTLength):
         lut_adr_l = i & (int)(2**(math.log(64,2)-2)-1) 
         lut_adr_h = (i >> (int)(math.log(64,2)-2)) & 0x3 #2bit
         lut_adr = lut_adr_h * lut_adr_l #
         wn = wn_lut((int)(math.log(FFTLength/64,2)),lut_adr)
         #print(wn)
-        stg1_tw.append(ComplexMul(stg1_bf[i],wn,DW_IN+2+2))
+        stg1_tw.append(ComplexMul(stg1_bf[i],wn,DW_IN+1+2))
+        stg1_wnid.append(lut_adr)
+        stg1_wn.append(wn)
+
+    print("stg1_wnid0",stg1_wnid[0:64:4])
+    print("stg1_wnid1",stg1_wnid[1:64:4])
+    print("stg1_wnid2",stg1_wnid[2:64:4])
+    print("stg1_wnid3",stg1_wnid[3:64:4])
+
+    print("stg2 tw",stg1_tw)
+    idx=0;
+    for i in stg1_wn[0:64:4]:
+        print(idx, " : stg2_twi_lut0 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg1_wn[1:64:4]:
+        print(idx, " : stg2_twi_lut1 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg1_wn[2:64:4]:
+        print(idx, " : stg2_twi_lut2 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg1_wn[3:64:4]:
+        print(idx, " : stg2_twi_lut3 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
 
     stg2_bf = [0]*128
     for i in range(0,int(FFTLength/16)):
@@ -263,15 +339,48 @@ def fft128(fft_in):
            stg2_bf[i*16+j+4]  = res[1]
            stg2_bf[i*16+j+8]  = res[2]
            stg2_bf[i*16+j+12] = res[3]
+           #print("stg3 bf in", stg1_tw[i*16+j],stg1_tw[i*16+j+4],stg1_tw[i*16+j+8],stg1_tw[i*16+j+12])
 
     stg2_tw =[]
+    stg2_wnid =[]
+    stg2_wn =[]
+    print("stg3 bf",stg2_bf)
     for i in range(0,FFTLength):
         lut_adr_l = i & (int)(2**(math.log(16,2)-2)-1) 
         lut_adr_h = (i >> (int)(math.log(16,2)-2)) & 0x3 #2bit
         lut_adr = lut_adr_h * lut_adr_l #0 0 0 0 , 0 1 2 3, 0 2 4 6, 0 0 0 0, 0 1 2 3。。。
         wn = wn_lut((int)(math.log(FFTLength/16,2)),lut_adr)
         #print(wn)
-        stg2_tw.append(ComplexMul(stg2_bf[i],wn,DW_IN+2+2+2))
+        stg2_tw.append(ComplexMul(stg2_bf[i],wn,DW_IN+1+2+2))
+        stg2_wnid.append(lut_adr)
+        stg2_wn.append(wn)
+    
+    print("stg3 tw",stg2_tw)
+    #print("stg2_wnid0",stg2_wnid[0:16:4])
+    #print("stg2_wnid1",stg2_wnid[1:16:4])
+    #print("stg2_wnid2",stg2_wnid[2:16:4])
+    #print("stg2_wnid3",stg2_wnid[3:16:4])
+
+    idx=0;
+    for i in stg2_wn[0:16:4]:
+        #print(idx, " : stg3_twi_lut0 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg2_wn[1:16:4]:
+        #print(idx, " : stg3_twi_lut1 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg2_wn[2:16:4]:
+        #print(idx, " : stg3_twi_lut2 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
+    idx=0;
+    for i in stg2_wn[3:16:4]:
+        #print(idx, " : stg3_twi_lut3 = {", "+"if(i.real>=0)else"-","14\'d",(int)(abs(i.real)),",", "+"if(i.imag>=0)else"-","14\'d",(int)(abs(i.imag)),"};")
+        idx=idx+1
+
 
     stg3_bf =[]
     for i in range(0,int(FFTLength/4)):
@@ -280,6 +389,7 @@ def fft128(fft_in):
         stg3_bf.append(res[1])
         stg3_bf.append(res[2])
         stg3_bf.append(res[3])
+    print("stg4 bf",stg3_bf)
 
     #last , mul 1/sqrt(fft_length)
     fft_out = [0]*128 #DW_IN+8 bit
@@ -292,16 +402,23 @@ def fft128(fft_in):
         out_imag = clip(out_imag , DW_IN)
         fft_out[i] = complex(out_real,out_imag)
 
+
      #reorder out 
-        fft_out_reorder=[]
-        for i in range(0,128):
-            k1 = i & 0x1
-            k2 = (i >> 1) & 0x3
-            k3 = (i >> 3) & 0x3
-            k4 = (i >> 5) & 0x3
-            #new_i = k1*4*4*2 + k2*4*2 + k3*2 + k4 
-            new_i = k1*4*4*4 + k2*4*4 + k3*4 + k4 
-            fft_out_reorder.append(fft_out[new_i])
+    fft_out_reorder=[]
+    disorder=[]
+    for i in range(0,128):
+        k1 = i & 0x1
+        k2 = (i >> 1) & 0x3
+        k3 = (i >> 3) & 0x3
+        k4 = (i >> 5) & 0x3
+        #new_i = k1*4*4*2 + k2*4*2 + k3*2 + k4 
+        new_i = k1*4*4*4 + k2*4*4 + k3*4 + k4 
+        disorder.append(new_i)
+        fft_out_reorder.append(fft_out[new_i])
+
+    print("disorder",disorder)
+
+    print("fft_disorder out",fft_out)
 
     return(fft_out_reorder)
 
@@ -318,20 +435,22 @@ def ifft128(fft_in):
     return ifft_out
 
 #fft_out = fft128(fft_in)
-fft_out = fft128(ffyiq)
-print(fft_out)
-print(ifft128(fft_out))
-N=128
-x = np.arange(N)           # 频率个数
-abs_y=np.abs(fft_out)                # 取复数的绝对值，即复数的模(双边频谱)
-angle_y=np.angle(fft_out)              #取复数的角度
- 
-plt.figure()
-plt.plot(x,abs_y)   
-plt.title('双边振幅谱（未归一化）')
- 
-plt.figure()
-plt.plot(x,angle_y)   
-plt.title('双边相位谱（未归一化）')
-plt.show()
+fft_out = fft128(fft_orderdata)
+
+#fft_out = fft128(ffyiq)
+#print(fft_out)
+#print(ifft128(fft_out))
+#N=128
+#x = np.arange(N)           # 频率个数
+#abs_y=np.abs(fft_out)                # 取复数的绝对值，即复数的模(双边频谱)
+#angle_y=np.angle(fft_out)              #取复数的角度
+# 
+#plt.figure()
+#plt.plot(x,abs_y)   
+#plt.title('双边振幅谱（未归一化）')
+# 
+#plt.figure()
+#plt.plot(x,angle_y)   
+#plt.title('双边相位谱（未归一化）')
+#plt.show()
    
